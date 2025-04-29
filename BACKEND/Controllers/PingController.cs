89b87718b1ecdb8a4115d1e696f94d7811a136c1
Ármai Zsolt <armaizsolt@hotmail.com>
2025-04-29 21:41:07 +0200

    add project setup

---
+++ BACKEND/Controllers/PingController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.NetworkInformation;
+using Szolgaltatas_ellenorzese.Models;
+
+namespace Szolgaltatas_ellenorzese.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<PingResult>> Post(string url) 
+        { 
+            var result = new PingResult();
+            var pingSender = new Ping();
+
+            var reply = await pingSender.SendPingAsync(url);
+
+            result.Url = url;
+            result.IsLive = reply.Status == IPStatus.Success;
+
+            return result;
+        }
+    }
+}
---
+++ BACKEND/Models/PingResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Szolgaltatas_ellenorzese.Models
+{
+    public class PingResult
+    {
+        public string Url { get; set; }
+        public bool IsLive { get; set; }
+    }
+}
