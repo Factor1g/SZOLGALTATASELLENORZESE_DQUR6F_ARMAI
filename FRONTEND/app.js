3c0072f0941405d057e43ad03069456e2d4ea6da
Ármai Zsolt <armaizsolt@hotmail.com>
2025-05-02 11:35:41 +0200

    Add simple JS to fill static result table

--- FRONTEND/app.js
+++ FRONTEND/app.js
@@ -2,7 +2,7 @@ document.getElementById('urlForm').addEventListener('submit', async function(e) 
 	e.preventDefault();
 
 	const urls = document.getElementById('urls').value.trim().split('\n').filter(Boolean)
-	const response = await fetch('https://localhost:5176/api/ping', {
+	const response = await fetch('https://localhost:7212/api/ping', {
 		method: 'POST',
 		headers: { 'Content-Type': 'application/json' },
 		body: JSON.stringify(urls)
@@ -11,6 +11,16 @@ document.getElementById('urlForm').addEventListener('submit', async function(e) 
 	const data = await response.json();
 
 	console.log("Backend válasz:", data);
+
+	const row = document.getElementById('row-1');
+	const item = data[0];
+	row.innerHTML = `
+	<td>${item.url}</td>
+            <td>${item.isLive ? 'Elérhetõ' : 'Nem elérhetõ'}</td>
+            <td>${item.address}</td>
+            <td>${item.roundtripTime}</td>
+            <td>${item.ttl}</td>
+            <td>${item.bufferSize}</td>`
 });
 
 
